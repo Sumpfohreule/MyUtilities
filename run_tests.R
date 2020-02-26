919820d0f34b09562eb19f3595b5ed4bbf3d677e
Lars Isenberg <Lars.Isenberg@forst.bwl.de>
2020-02-26 10:52:20 +0100

    moved RUnit tests into test folder for better structure

--- run_tests.R
+++ run_tests.R
@@ -2,7 +2,7 @@
 devtools::load_all()
 
 ########################################################################################################################
-full_test_suite <- RUnit::defineTestSuite("Full test suite", dirs = "RUnitTests")
+full_test_suite <- RUnit::defineTestSuite("Full test suite", dirs = "tests/RUnitTests")
 RUnit::isValidTestSuite(full_test_suite)
 runit <- RUnit::runTestSuite(full_test_suite)
 RUnit::printTextProtocol(runit)
