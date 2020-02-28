0bb3978691820207d46a019b7c9e2bd1c9dccd2b
Lars Isenberg <Lars.Isenberg@forst.bwl.de>
2020-02-28 11:49:27 +0100

    interactive upgrade question and changed parameter order for convenience (Major Change)

--- R/updateCustomPackage.R
+++ R/updateCustomPackage.R
@@ -6,12 +6,12 @@
 #' @param package.location location/name of the package. Can be an indirect path
 #' @export
 #'
-updateCustomPackage <- function(package.location = getwd(), incrementing_level = "Development") {
+updateCustomPackage <- function(incrementing_level = "Development", package.location = getwd()) {
     incrementPackageVersion(package.location, incrementing_level = incrementing_level)
     original.warn.value <- options("warn")[[1]]
     options(warn = 1)
     devtools::document(package.location)
     devtools::build(package.location)
-    devtools::install(package.location)
+    devtools::install(package.location, upgrade = FALSE)
     options(warn = original.warn.value)
 }
