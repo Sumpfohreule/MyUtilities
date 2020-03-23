3bc23d6863321d1b58129d33c340a5001344fe53
Lars Isenberg <isenberg.lars@gmail.com>
2020-03-23 14:24:02 +0100

    finalized interlace function

--- R/interlace.R
+++ R/interlace.R
@@ -1,4 +1,16 @@
+#' Combine two vectors in alternating order
+#'
+#' A new vector is created which contains the values of both input vectors in alternating order beginning with the first input vector
+#'
+#' @param vector_1,vector_2 Any vector which is compatible with the other or coercible
+#' @export
 interlace <- function(vector_1, vector_2) {
+    if (is.null(vector_1)) {
+        return(vector_2)
+    } else if (is.null(vector_2)) {
+        return(vector_1)
+    }
+
     v1_len <- length(vector_1)
     v2_len <- length(vector_2)
     min_length <- min(v1_len, v2_len)
@@ -8,7 +20,7 @@ interlace <- function(vector_1, vector_2) {
     out_vector <- c(vector_1, vector_2)[index_sequence]
 
     if (v1_len > v2_len) {
-        add_index <- (min_length + 1):(min_length + v1_len)
+        add_index <- (min_length + 1):v1_len
         out_vector <- c(out_vector, vector_1[add_index])
     } else if (v2_len > v1_len) {
         add_index <- (min_length + 1):v2_len
--- tests/testthat/test-interlace.R
+++ tests/testthat/test-interlace.R
@@ -18,3 +18,31 @@ test_that("First vector is shorter multiple times", {
 
     expect_equal(interlace(vec_1, vec_2), c(1, 4, 5, 6, 8, 10, 20, 30, 40, 50, 60))
 })
+
+test_that("Second vector is multiples shorter", {
+    vec_1 <- c(4, 6, 8, 10, 20, 30, 40, 50, 60)
+    vec_2 <- c(1, 5)
+
+    expect_equal(interlace(vec_1, vec_2), c(4, 1, 6, 5, 8, 10, 20, 30, 40, 50, 60))
+})
+
+test_that("First vector is empty", {
+    vec_1 <- c()
+    vec_2 <- c(1, 5, 10)
+
+    expect_equal(interlace(vec_1, vec_2), c(1, 5, 10))
+})
+
+test_that("Second vector is empty", {
+    vec_1 <- c(1, 5, 10)
+    vec_2 <- c()
+
+    expect_equal(interlace(vec_1, vec_2), c(1, 5, 10))
+})
+
+test_that("Both vectors are empty", {
+    vec_1 <- c()
+    vec_2 <- c()
+
+    expect_equal(interlace(vec_1, vec_2), c())
+})
