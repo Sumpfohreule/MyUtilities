204323b2367b6185b4c877e331fc8579c30a3ff5
Lars Isenberg <isenberg.lars@gmail.com>
2020-04-16 16:42:41 +0200

    Added function to fill gaps for a whole data.frame

--- R/fillDateGaps.R
+++ R/fillDateGaps.R
@@ -1,16 +1,16 @@
 ########################################################################################################################
 # TODO: make fillDateGaps.default a real default method or maybe just use seq to overload
 #' Generic S3 Method for filling in date times
-#' 
+#'
 #' Missing value within a date/time vector are filled according to a provided interval
-#' 
-#' @export 
-#' 
-fillDateGaps <- function(obj) {
+#'
+#' @export
+#'
+fillDateGaps <- function(obj, ...) {
     UseMethod("fillDateGaps")
 }
 
 fillDateGaps.default <- function(obj) {
-    stop("Only S3 method implementations for the following classes exist:\n\t", 
+    stop("Only S3 method implementations for the following classes exist:\n\t",
         paste0(methods(fillDateGaps), collapse = "\n\t"))
 }
---
+++ R/fillDateGaps.data.frame.R
@@ -0,0 +1,55 @@
+#' Fills a data.frame with missing dates
+#'
+#' For each grouping column variation defined by group_columns the specific date intervall is calculated and missing dates are filled
+#' into the data.frame
+#' @param data A data.frame containing at least a date column
+#' @param group_columns Optional: If the data is grouped, provide the grouping column names
+#' @param date_column Name of the date column. Can be omitted if the data.frame contains a single column of type POSIXct
+#' @param windwo If intervalls can change within their groups use a window ("year", "month", "day")
+#' to specify a finer net to calculate the filling intervalls
+#' @export
+fillDateGaps.data.frame <- function(data, group_columns = NULL, date_column = NULL, window = NULL) {
+    assertthat::assert_that(assertthat::is.string(date_column) || is.null(date_column))
+    data <- as.data.table(data)
+
+    if (is.null(date_column)) {
+        date_column <- data %>%
+            purrr::map(class) %>%
+            purrr::map(function(x) "POSIXt" %in% x) %>%
+            purrr::keep(function(x) x) %>%
+            names()
+    }
+    if (length(date_column) < 1) {
+        stop("No date column found. Provide a date_column or convert the column to POSIXct")
+    } else if (length(date_column) > 1) {
+        stop("Multiple date columns found. Explicitly state the one to use in date_column")
+    }
+
+    window_options <- c("year", "month", "day")
+    if (!is.null(window) && (window %in% window_options)) {
+        additional_groups <- window_options[1:which(window_options %in% window)]
+        for (new_group in additional_groups) {
+            window_function <- get(new_group)
+            data[, (new_group) := window_function(get(date_column))]
+        }
+        group_columns <- c(group_columns, additional_groups)
+    }
+
+    key_vector <- c(group_columns, date_column)
+    setkeyv(data, key_vector)
+
+    if (is.null(group_columns)) {
+        new_dates <- data[, .(dates = fillDateGaps(get(date_column)))]
+    } else {
+        new_dates <- data[, .(dates = fillDateGaps(get(date_column))), by = c(group_columns)]
+    }
+    setnames(new_dates,
+             old = "dates",
+             new = date_column)
+    setkeyv(new_dates, key_vector)
+
+    filled_data <- merge(new_dates, data, by = key_vector, all.x = TRUE)
+    filled_data <- filled_data %>%
+        select(-additional_groups)
+    return(filled_data)
+}
