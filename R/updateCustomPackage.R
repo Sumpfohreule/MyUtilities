8fe9a406428653f0e79eba2292777ae91d35ffc8
Lars Isenberg <isenberg.lars@gmail.com>
2020-04-27 13:52:56 +0200

    New option for updateCustomPackage to not update VersionNumber (new Default)

--- R/updateCustomPackage.R
+++ R/updateCustomPackage.R
@@ -3,11 +3,14 @@
 #'
 #' Wraps functions for documenting and installing packages in a short command
 #'
+#' @param incrementing_level If Version number of this Package should be increased (one of "Development", "Patch", "Minor", "Major")
 #' @param package.location location/name of the package. Can be an indirect path
 #' @export
 #'
-updateCustomPackage <- function(incrementing_level = "Development", package.location = getwd()) {
-    incrementPackageVersion(package.location, incrementing_level = incrementing_level)
+updateCustomPackage <- function(incrementing_level = "None", package.location = getwd()) {
+    if (incrementing_level %in% c("Development", "Patch", "Minor", "Major")) {
+        incrementPackageVersion(package.location, incrementing_level = incrementing_level)
+    }
     original.warn.value <- options("warn")[[1]]
     options(warn = 1)
     devtools::document(package.location)
