f3029ac1c4077c356a1189cb390199a3e8f6f3ec
Lars Isenberg <isenberg.lars@gmail.com>
2020-11-03 14:19:50 +0100

    Reapply a lot of changes

--- R/importAggregateExcelSheet.R
+++ R/importAggregateExcelSheet.R
@@ -1,42 +1,35 @@
 #' Imports FVA specific Level2 aggregated xlsx-file (Gesamt)
 #'
-#' @param xlsx_path A file path to an excel with a specific structure (Two header columns; date column named 'Datum')
+#' @param xlsx_path A file path (or a workbook) to an excel with a specific structure (Two header columns; date column named 'Datum')
 #' @param sheet Single sheet name of the excel file to be imported
 #' @return A data.table
 #' @export
 #'
 importAggregateExcelSheet <- function(xlsx_path, sheet) {
-    tryCatch({
-        input_sheet <- openxlsx::read.xlsx(
-            xlsx_path,
-            sheet = sheet)
-        names(input_sheet) <- stringr::str_replace(names(input_sheet), pattern = "^.*Datum$", replacement = "Datum")
-        original_names <- names(input_sheet)
+    if (assertthat::is.string(xlsx_path)) {
+        assertthat::assert_that(is_contained(sheet, openxlsx::getSheetNames(xlsx_path)))
+    } else {
+        assertthat::assert_that(is_contained(sheet, names(xlsx_path)))
+    }
+    input_sheet <- openxlsx::read.xlsx(
+        xlsx_path,
+        sheet = sheet)
+    names(input_sheet) <- stringr::str_replace(names(input_sheet), pattern = "^.*Datum.*$", replacement = "Datum")
+    original_names <- names(input_sheet)
 
-        date_col <- sym("Datum")
-        output_sheet <- input_sheet %>%
-            as_tibble(.name_repair = "unique") %>%
-            slice(-1) %>%
-            purrr::map(function(x) {
-                col_type <- readr::guess_parser(x)
-                conversion_function <- get(paste0("as.", col_type))
-                return(conversion_function(x))
-            }) %>%
-            as_tibble() %>%
-            mutate(!!date_col := as.POSIXct(
-                !!date_col * 60 * 60 * 24,
-                origin = "1899-12-30",
-                tz = "UTC")) %>%
-            mutate(!!date_col := roundPOSIXct(
-                !!date_col,
-                in.seconds = 5 * 60,
-                round.fun = round)) %>%
-            data.table::as.data.table()
-    }, error = function(e) {
-        message <- geterrmessage()
-        if (!stringr::str_detect(message, "Cannot find sheet named"))
-            stop(e)
-    })
+    date_col <- sym("Datum")
+    output_sheet <- input_sheet %>%
+        as_tibble(.name_repair = "unique") %>%
+        slice(-1) %>%
+        purrr::map(function(x) {
+            col_type <- readr::guess_parser(x)
+            conversion_function <- get(paste0("as.", col_type))
+            return(conversion_function(x))
+        }) %>%
+        as_tibble() %>%
+        mutate(!!date_col := as.POSIXct(!!date_col * 60 * 60 * 24, origin = "1899-12-30", tz = "UTC")) %>%
+        mutate(!!date_col := lubridate::round_date(!!date_col, "5min")) %>%
+        data.table::as.data.table()
     names(output_sheet) <- original_names
     return(output_sheet)
 }
--- R/isLeapYear.R
+++ R/isLeapYear.R
@@ -1,24 +1,25 @@
 ########################################################################################################################
 #' Checks if a given year is a leap year
-#' 
+#'
 #' @description A simple function determining if a given numeric value is a leap year
-#' 
+#'
 #' @param year A numeric value which represents a year
 #' @keywords dates
-#' @export 
-#' @examples 
+#' @export
+#' @examples
 #' # is leap year:
 #' isLeapYear(1996)
-#' 
+#'
 #' # not leap year(1997)
 #' isLeapYear(1997)
-#' 
+#'
 #' # not leap year (devisable by 100 but not 400)
 #' isLeapYear(1900)
-#' 
+#'
 #' # is leap year (devisable by 400)
 #' isLeapYear(2000)
-#' 
+#'
 isLeapYear <- function(year) {
+    .Deprecated("lubridate::leap_year")
     return (year %% 400 == 0 | (year %% 4 == 0 & !year %% 100 == 0))
 }
