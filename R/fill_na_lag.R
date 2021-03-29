c059537507e4e91248ca195a771a9b4f21623161
Lars Isenberg <isenberg.lars@gmail.com>
2021-03-29 16:51:16 +0200

    Further improved fill_na_lag with added test cases

--- R/fill_na_lag.R
+++ R/fill_na_lag.R
@@ -1,13 +1,22 @@
+#' Fills NAs within a vector with the last proper value, which came before it
+#'
+#' NAs which start before any value are left as is.
+#' The same goes for vectors only containing NA
+#'
+#' @param values Any vector
+#' @export
 fill_na_lag <- function(values) {
-    assertthat::assert_that(!all(is.na(values)), msg = "Can't fill if only NAs are contained")
     is_na <- is.na(values)
     value_index <- which(!is_na)
     na_index <- which(is_na)
-    replacement_index <- na_index %>%
-        purrr::map(~ max(value_index[value_index < .x])) %>%
-        unlist()
+    if (all(length(value_index) != 0, length(na_index) != 0)) {
+        na_index <- na_index %>%
+            purrr::keep(~ .x > min(value_index))
+        replacement_index <-  na_index %>%
+            purrr::map(~ max(value_index[value_index < .x])) %>%
+            unlist()
 
-
-    values[na_index] <- values[replacement_index]
+        values[na_index] <- values[replacement_index]
+    }
     return(values)
 }
