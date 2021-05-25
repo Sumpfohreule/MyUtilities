1cc182ba5c4c4913948b62f6e11af6d20b80e6e8
Lars Isenberg <isenberg.lars@gmail.com>
2021-05-25 11:50:26 +0200

    Deprecated as.POSIXctFixed as lubridate works better and uses UTC bydefault

--- R/as.POSIXctFixed.R
+++ R/as.POSIXctFixed.R
@@ -1,13 +1,14 @@
 ########################################################################################################################
 #' Fix for as.POSXct not accepting tz
-#' 
+#'
 #' This small fix allows usage of the timezone argument for Date objects.
-#' 
+#'
 #' @param x A Date value to be converted to POSIXct
 #' @param ... Optional values including tz for specifying time zone
-#' @export 
-#' 
+#' @export
+#'
 as.POSIXctFixed <- function(x, ...) {
+    .Deprecated(new = "lubridate::as_datetime")
     if ("Date" %in% class(x)) {
         return(.POSIXct(unclass(x) * 86400, ...))
     } else {
