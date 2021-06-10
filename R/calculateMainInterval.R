dd97352c2312dc8b0485520bb445b3c8b1caaeb0
Lars Isenberg <isenberg.lars@gmail.com>
2021-06-10 15:23:38 +0200

    Anticipated some more edge cases for calculateMainInterval which are now taken care of

--- R/calculateMainInterval.R
+++ R/calculateMainInterval.R
@@ -1,17 +1,19 @@
 ########################################################################################################################
-# FIXME: catch single value (and missing value / non POSIXct)
-# TODO: Check how this function handles if multiple intervals are equaly frequent
 #' Find the most prevalent interval in a vector
-#' 
-#' The intervall between all neighbouring vector values is calculated and the most frequent one returned.
+#'
+#' The interval between all neighbouring vector values is calculated and the most frequent one returned.
 #' Can be used to find the actual logging interval between dates if there are gaps in the vector
-#' 
+#'
 #' @param vector A vector of values which must be convertible to numeric (dates, date-times, etc.)
-#' @export 
-#' 
+#' @export
+#'
 calculateMainInterval <- function(vector) {
+    assertthat::assert_that(length(vector) > 1)
     vector <- as.numericTryCatch(vector)
     diff.counts <- table(diff(as.numeric(vector)))
     diff.value <- attr(diff.counts[diff.counts == max(diff.counts)], "names")
+    if (length(diff.value) > 1) {
+        stop("There are multiple intervals with the same frequency")
+    }
     return(as.numeric(diff.value))
 }
