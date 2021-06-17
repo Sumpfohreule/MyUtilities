36ef3a92a2a74d868dd553872901fc3306a9dd5a
Lars Isenberg <isenberg.lars@gmail.com>
2021-06-17 17:21:57 +0200

    Added error message to fillGaps as it is not yet working as intended

--- R/fillGaps.R
+++ R/fillGaps.R
@@ -11,6 +11,7 @@
 #' @param interval A numeric value describing the expected interval
 #' @export
 fillGaps <- function(values, interval) {
+    stop("Not working correctly yet!")
     assertthat::assert_that(length(interval) == 1)
     local_extrema <- .findLocalExtrema(values)
     purrr::map2(.x = values[local_extrema][-length(local_extrema)],
