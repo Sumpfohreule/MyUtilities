6e79604643842111439dba15044e0d7200d45194
Lars Isenberg <lars.isenberg@forst.bwl.de>
2021-11-17 20:37:14 +0100

    Added documentation to keep_names

--- R/keep_names.R
+++ R/keep_names.R
@@ -1,3 +1,11 @@
+#' A purrr like function to keep only elements with certain names
+#'
+#' @param .x A list or atomic vector
+#' @param .p A predicate function similar to purrr::keep.
+#' Only element names which evaluate to TRUE with this function will be kept
+#' @param ... Additional parameters passed to .p
+#' @export
+#'
 keep_names <- function(.x, .p, ...) {
     keep_vector <- names(.x) %>%
         purrr::map_lgl(.p, ...)
